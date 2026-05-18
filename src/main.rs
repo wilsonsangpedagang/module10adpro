9047ed9238d6b819dc77d643ced9801c336be491
wilsonsangpedagang <davinmagic@gmail.com>
2026-05-18 20:01:03 +0700

    Experiment 1.1: Original timer from the book.

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let (executor, spawner) = new_executor_and_spawner();
+
+    // Spawn a task to print before and after waiting on a timer.
+    spawner.spawn(async {
+        println!("Davin's Komputer: howdy!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Davin's Komputer: done!");
+    });
+
+    // Drop the spawner so that our executor knows it is finished and won't
+    // receive more incoming tasks to run.
+    drop(spawner);
+
+    // Run the executor until the task queue is empty.
+    executor.run();
+}
